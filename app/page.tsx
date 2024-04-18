4f30e63829a734d72db2bf794165117ad85f9e50
David-MacD <david-p-macdonald@outlook.com>
2024-04-18 10:24:10 +0100

    Create the skeleton of the portfolio main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,37 @@
-import React from "react";
-import styles from "./page.module.css";
+import Link from "next/link";
+import s from "./page.module.css";
+// import { cx } from "@/lib/util";
+// import css from "./util.module.css";
 
 export default function Home() {
 	return (
-		<main className={styles.main}>
-			<div className={styles.message}>Portfolio for David!</div>
+		<main className={s.main}>
+			<header className={s.header}>
+				<Link href="/" className={s.brand}>
+					David MacDonald
+				</Link>
+				<ul className={s.nav}>
+					<li className={s.navLink}>
+						<Link href="/rojects" className={s.navLink}>
+							Projects
+						</Link>
+					</li>
+					<li className={s.navLink}>
+						<Link href="/contact" className={s.navLink}>
+							Contact
+						</Link>
+					</li>
+				</ul>
+			</header>
+			<section className={s.summary}>
+				<span>Summary</span>
+			</section>
+			<section className={s.projects}>
+				<span>Projects</span>
+			</section>
+			<footer className={s.mainFooter}>
+				<span>Footer</span>
+			</footer>
 		</main>
 	);
 }
---
+++ lib/util.ts
@@ -0,0 +1,3 @@
+export function cx(...classes: string[]) {
+	return classes.join(" ");
+}
