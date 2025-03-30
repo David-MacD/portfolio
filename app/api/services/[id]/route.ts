da7179ce0e4ad330c5cb0bf25a2c07c7ecaa6727
David-MacD <149108133+David-MacD@users.noreply.github.com>
2025-03-30 02:25:25 +0100

    Fix Route Handler
    - Params are now async in Next 15

--- app/api/services/[id]/route.ts
+++ app/api/services/[id]/route.ts
@@ -1,9 +1,9 @@
-type ReqOptions = { params: { id: string } };
+type Params = Promise<{ id: string }>;
 
 const getNumberBetween = (min: number, max: number) =>
   min + Math.floor(Math.random() * (max - min + 1));
 
-export async function GET(req: Request, { params }: ReqOptions) {
+export async function GET(req: Request, data: { params: Params }) {
   if (getNumberBetween(1, 10) > 5) {
     return new Response("Unavailable", { status: 503 });
   }
